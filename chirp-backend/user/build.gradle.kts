b7159d7f9346880f2ad6f7674388cc2da6ffbc4d
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-09-14 12:44:17 +0530

    Update dependencies

--- chirp-backend/user/build.gradle.kts
+++ chirp-backend/user/build.gradle.kts
@@ -15,6 +15,10 @@ repositories {
 
 dependencies {
     implementation(projects.common)
+
+    implementation(libs.spring.boot.starter.data.jpa)
+    runtimeOnly(libs.postgresql)
+
     testImplementation(kotlin("test"))
 }
 
