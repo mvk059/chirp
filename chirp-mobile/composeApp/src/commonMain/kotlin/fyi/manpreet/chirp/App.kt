738279dd07161a87b028a9e981e5606608ddbc7a
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-10-19 15:54:01 +0530

    Setup theming

--- chirp-mobile/composeApp/src/commonMain/kotlin/fyi/manpreet/chirp/App.kt
+++ chirp-mobile/composeApp/src/commonMain/kotlin/fyi/manpreet/chirp/App.kt
@@ -10,19 +10,23 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import chirp.composeapp.generated.resources.Res
 import chirp.composeapp.generated.resources.compose_multiplatform
+import fyi.manpreet.core.designsystem.theme.ChirpTheme
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    ChirpTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
